2347ac9734f060246ea94c48e948234286c81988
BritvA <apan1998@yandex.ru>
2023-03-09 21:54:10 +0300

    исправлены ошибки

--- src/Laboratory/Lab05/Task02.java
+++ src/Laboratory/Lab05/Task02.java
@@ -7,26 +7,14 @@ public class Task02 {
         String word = "Довод";
 
         System.out.println("Слово \'" + word + "\' "
-                            + (isPalindrome2(word) ? "является" : "не является")
+                            + (isPalindrome(word) ? "является" : "не является")
                             + " палиндромом");
     }
 
-    public static boolean isPalindrome2(String word) {
+    public static boolean isPalindrome(String word) {
         word = word.toLowerCase();
-        String left = "";
-        String right = "";
-        int wordLength = word.length();
+        StringBuilder builder = new StringBuilder(word);
 
-        if (wordLength % 2 == 0) {
-            left = word.substring(0, wordLength / 2);
-            right = word.substring(wordLength / 2);
-        } else {
-            left = word.substring(0, wordLength / 2);
-            right = word.substring(wordLength / 2 + 1);
-        }
-
-        StringBuilder rightBuilder = new StringBuilder(right);
-
-        return left.equals(rightBuilder.reverse().toString());
+        return word.equals(builder.reverse().toString());
     }
 }
--- src/Laboratory/Lab05/Task03.java
+++ src/Laboratory/Lab05/Task03.java
@@ -3,12 +3,14 @@ package Laboratory.Lab05;
 public class Task03 {
     public static void main(String[] args) {
         String text = "Слово бяка - это что-то плохое";
+        String modifiedWord = "бяка";
+        String replacement = "[вырезано цензурой]";
 
         System.out.println(text);
-        System.out.println(replaceWord(text));
+        System.out.println(replaceWord(text, modifiedWord, replacement));
     }
 
-    private static String replaceWord(String text) {
-        return text.replaceAll("бяка", "[вырезано цензурой]");
+    private static String replaceWord(String text, String modifiedWord, String replacement) {
+        return text.replaceAll(modifiedWord, replacement);
     }
 }
