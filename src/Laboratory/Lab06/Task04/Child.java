5514c73946a9427d0732569e23218923cec6b6db
BritvA <apan1998@yandex.ru>
2023-03-11 15:13:16 +0300

    добавлено считывание числа с консоли в базовом классе

--- src/Laboratory/Lab06/Task04/Child.java
+++ src/Laboratory/Lab06/Task04/Child.java
@@ -1,10 +1,6 @@
 package Laboratory.Lab06.Task04;
 
 public class Child extends Parent {
-    public Child(Integer number) {
-        super(number);
-    }
-
     public void printNumber() {
         System.out.println(super.number);
     }
--- src/Laboratory/Lab06/Task04/Parent.java
+++ src/Laboratory/Lab06/Task04/Parent.java
@@ -1,9 +1,12 @@
 package Laboratory.Lab06.Task04;
 
+import java.util.Scanner;
+
 public class Parent {
     public Integer number;
 
-    public Parent(Integer number) {
-        this.number = number;
+    public Parent() {
+        Scanner scanner = new Scanner(System.in);
+        number = scanner.nextInt();
     }
 }
